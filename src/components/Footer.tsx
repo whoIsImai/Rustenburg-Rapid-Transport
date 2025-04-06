5540eb24874e3a24508c17241c2a53b92106a842
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-06 18:11:19 +0200

    removed a link from the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,11 +31,6 @@ export default function Footer() {
                   </Link>
                 </li>
                 <li>
-                  <Link to="/fleet" className="text-sm text-white/80 hover:text-white transition-colors">
-                    Our Fleet
-                  </Link>
-                </li>
-                <li>
                   <Link to="/book" className="text-sm text-white/80 hover:text-white transition-colors">
                     Book Online
                   </Link>
